b06c5001d13f9617c4e5a95235fea6424e1a273e
Saad Tariq <saadtariq232@gmail.com>
2025-01-09 14:33:46 +0500

    modified ner_service.py

--- ner_service.py
+++ ner_service.py
@@ -1,7 +1,15 @@
 import spacy
+import subprocess
 
-# Load spaCy's pre-trained model for NER
-NER = spacy.load("en_core_web_sm")
+# Check if the spaCy model is available, if not, install it
+try:
+    # Try loading the model
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    # Download the model using subprocess if it's not installed
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"], check=True)
+    # Load spaCy's pre-trained model for NER
+    NER = spacy.load("en_core_web_sm")
 
 def extract_company_name_from_text(text):
     """
@@ -10,3 +18,7 @@ def extract_company_name_from_text(text):
     doc = NER(text)  # Run NER on the transcribed text
     companies = [ent.text for ent in doc.ents if ent.label_ == "ORG"]
     return companies
+
+
+
+
